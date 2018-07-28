284e8e4cf1493f7c35cb7e65f3c08ab23c8dbd4e
karol <karol.kujawa85@gmail.com>
2018-07-28 23:08:39 +0200

    first euler problem

--- euler_nr1.py
+++ euler_nr1.py
@@ -1,9 +1,6 @@
-c = 1000
-
-for x in range(1, c + 1):
-
+for x in range(1, 1001):
     if x % 3 and x % 5:
-
         continue
-
     print(x)
+
+"print('\n'.join(str(n) for n in range(1, 1001) if n % 5 == 0 or n % 3 == 0))"
