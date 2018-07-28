cfd4d3000740f11257a2dabdbcc26b1077972b93
karol <karol.kujawa85@gmail.com>
2018-07-29 00:19:22 +0200

    first euler problem

--- euler_nr2.py
+++ euler_nr2.py
@@ -1,9 +1,7 @@
 from math import sqrt
 
-
 def F(n):
     return ((1 + sqrt(5)) ** n - (1 - sqrt(5)) ** n) / (2 ** n * sqrt(5))
-
 
 def F():
     a, b = 1, 2
@@ -11,14 +9,12 @@ def F():
         yield a
         a, b = b, a + b
 
-
 def SF(startNr, endNr):
     for cur in F():
         if cur > endNr:
             return
         if cur >= startNr:
             yield cur
-
 
 for i in SF(0, 4000000):
    if i % 2 == 0:
